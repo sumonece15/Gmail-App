b8f4659fbaa88f14670072cbbd74226b6da49f94
Sumon <sumonece15@gmail.com>
2021-06-01 01:54:20 +0600

    Gmail App (1.0)

---
+++ app/src/main/java/com/sumon/gmailapp/navigation/LabelItem.java
@@ -0,0 +1,18 @@
+package com.sumon.gmailapp.navigation;
+
+public class LabelItem {
+
+    private String label;
+
+    public LabelItem(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+}
