0ca1f4b8498c3197d40978c4ed88417f29781376
Sumon <sumonece15@gmail.com>
2021-06-06 23:42:17 +0600

    Gmail App (1.0)

--- app/src/main/java/com/sumon/gmailapp/data/FakeDataSource.java
+++ app/src/main/java/com/sumon/gmailapp/data/FakeDataSource.java
@@ -34,7 +34,7 @@ public class FakeDataSource {
 
 
     // Dummy Mail Content
-    public static final String Content_1 = "Hi Sumon, Collect your I/O attendee";
+    public static final String Content_1 = "Hi Sumon, Collect your I/O attende";
 
     // Dummy Description
     public static final String DESC_1 = "Collect your I/O attendee Google Developer Profile badge";
